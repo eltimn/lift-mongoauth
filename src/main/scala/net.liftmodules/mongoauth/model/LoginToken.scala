ee5a7fc1eb8ee5290b3c8385644122031e1be4db
heralight <heralight@heirko.com>
2012-09-07 15:13:35 +0200

    update collectionname

--- src/main/scala/net.liftmodules/mongoauth/model/LoginToken.scala
+++ src/main/scala/net.liftmodules/mongoauth/model/LoginToken.scala
@@ -1,3 +1,27 @@
+/*******************************************************************************
+ * <copyright file="LoginToken.scala">
+ * Copyright (c) 2011 - 2012. Heirko
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Heirko and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Heirko
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Heirko.
+ *
+ * Heirko tout droit réservé -- Tout information contenue ici est la propriété
+ * de la société Heirko.
+ *
+ * </copyright>
+ *
+ * <author>Alexandre Richonnier</author>
+ * <lastUpdate>07/09/12 15:13</lastUpdate>
+ ******************************************************************************/
+
 package net.liftmodules.mongoauth
 package model
 
@@ -29,7 +53,7 @@ class LoginToken extends MongoRecord[LoginToken] with ObjectIdPk[LoginToken] {
 object LoginToken extends LoginToken with MongoMetaRecord[LoginToken] {
   import mongodb.BsonDSL._
 
-  final override def collectionName = "user.logintokens"
+  override val collectionName = "user.logintokens"
 
   ensureIndex((userId.name -> 1))
 
--- src/main/scala/net.liftmodules/mongoauth/model/Role.scala
+++ src/main/scala/net.liftmodules/mongoauth/model/Role.scala
@@ -1,3 +1,27 @@
+/*******************************************************************************
+ * <copyright file="Role.scala">
+ * Copyright (c) 2011 - 2012. Heirko
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Heirko and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Heirko
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Heirko.
+ *
+ * Heirko tout droit réservé -- Tout information contenue ici est la propriété
+ * de la société Heirko.
+ *
+ * </copyright>
+ *
+ * <author>Alexandre Richonnier</author>
+ * <lastUpdate>07/09/12 15:13</lastUpdate>
+ ******************************************************************************/
+
 package net.liftmodules.mongoauth
 package model
 
@@ -28,7 +52,7 @@ class Role private () extends MongoRecord[Role] {
   }
 }
 object Role extends Role with MongoMetaRecord[Role] {
-  final override def collectionName = "user.roles"
+  override def collectionName = "user.roles"
 
   def findOrCreate(in: String): Role = find(in).openOr(createRecord.id(in))
   def findOrCreateAndSave(in: String): Role = findOrCreate(in).save
