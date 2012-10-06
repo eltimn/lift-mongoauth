fc3c713b89b3007e70c54d569ecb89f6095b5d20
heralight <heralight@heirko.com>
2012-10-06 05:28:28 +0200

    fix waring in 25M1

--- src/main/scala/net.liftmodules/mongoauth/model/providers/AuthModule.scala
+++ src/main/scala/net.liftmodules/mongoauth/model/providers/AuthModule.scala
@@ -1,3 +1,27 @@
+/*******************************************************************************
+ * <copyright file="AuthModule.scala">
+ * Copyright (c) 2011 - 2012. Heirko
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Heirko and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Heirko
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Heirko.
+ *
+ * Heirko tout droit réservé -- Tout information contenue ici est la propriété
+ * de la société Heirko.
+ *
+ * </copyright>
+ *
+ * <author>Alexandre Richonnier</author>
+ * <lastUpdate>06/10/12 05:28</lastUpdate>
+ ******************************************************************************/
+
 package net.liftmodules.mongoauth
 package model
 package providers
@@ -35,7 +59,7 @@ object AuthModules extends  Logger {
 
   private var defAuth: Box[AuthModule] = Empty
 
-  def defaultAuthModule: AuthModule = defAuth.open_!
+  //def defaultAuthModule: AuthModule = defAuth.open_!
 }
 
 
